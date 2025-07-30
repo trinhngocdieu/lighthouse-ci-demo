292c93f1459907e6519c3163d2860b3d44d46160
Dieu <trinh.ngoc.dieu@moneyforward.co.jp>
2025-07-30 22:18:57 +0900

    chore: update

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,17 @@
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: './out',
+      numberOfRuns: 3,
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.8 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.8 }],
+        'categories:seo': ['warn', { minScore: 0.8 }],
+      },
+    },
     upload: {
       target: 'temporary-public-storage',
     },
