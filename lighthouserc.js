268927ff50988ba0b03ca872962f1a1a7089d369
Dieu <trinh.ngoc.dieu@moneyforward.co.jp>
2025-07-31 22:53:34 +0900

    chore: run every new pr

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,10 +6,10 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', { minScore: 0.8 }],
+        'categories:performance': ['error', { minScore: 0.8 }],
         'categories:accessibility': ['error', { minScore: 0.9 }],
-        'categories:best-practices': ['warn', { minScore: 0.8 }],
-        'categories:seo': ['warn', { minScore: 0.8 }],
+        'categories:best-practices': ['error', { minScore: 0.8 }],
+        'categories:seo': ['error', { minScore: 0.8 }],
       },
     },
     upload: {
